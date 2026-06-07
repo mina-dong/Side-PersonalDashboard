100d6e63db900517322a50cbb8118393b900fff8
Mina Dong <minadong404@gmail.com>
2026-06-08 00:50:42 +0900

    todos 리스트박스 바인딩 동작

---
+++ WpfApp1/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.Models
+{
+    internal class TodoItem
+    {
+        public string Title { get; set; }
+    }
+}
--- WpfApp1/ViewModels/MainViewModel.cs
+++ WpfApp1/ViewModels/MainViewModel.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using WpfApp1.Models;
 
 namespace WpfApp1.ViewModels
 {
     internal class MainViewModel
     {
        public string Title { get; set; }
-
+        
+       public ObservableCollection<TodoItem> Todos { get; set; } 
+       
         public MainViewModel() {
             Title = "Personal DashBoard";
+
+            Todos = new ObservableCollection<TodoItem> { 
+                new TodoItem {Title = "운동하기"},
+                new TodoItem {Title = "아침식사 준비하기"},
+                new TodoItem {Title = "세탁기 정리하기"},
+                    
+
+
+            };
+
         }
 
         
