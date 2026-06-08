791c4e68f45b3622758ec220f761331c0817828c
Mina Dong <minadong404@gmail.com>
2026-06-08 12:06:09 +0900

    Todo 추가(add) 기능

--- WpfApp1/ViewModels/MainViewModel.cs
+++ WpfApp1/ViewModels/MainViewModel.cs
@@ -5,15 +5,19 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Collections.ObjectModel;
 using WpfApp1.Models;
+using System.Windows.Input;
+using WpfApp1.Commands;
 
 namespace WpfApp1.ViewModels
 {
     internal class MainViewModel
     {
-       public string Title { get; set; }
-        
-       public ObservableCollection<TodoItem> Todos { get; set; } 
-       
+        public string Title { get; set; }
+        public string NewTodo { get; set; }
+        public ObservableCollection<TodoItem> Todos { get; set; }
+        public ICommand AddTodoCommand { get; set; }   
+
+    
         public MainViewModel() {
             Title = "Personal DashBoard";
 
@@ -21,13 +25,22 @@ namespace WpfApp1.ViewModels
                 new TodoItem {Title = "운동하기"},
                 new TodoItem {Title = "아침식사 준비하기"},
                 new TodoItem {Title = "세탁기 정리하기"},
-                    
-
-
             };
+
+            AddTodoCommand = new RelayCommand(AddTodo);
 
         }
 
+        private void AddTodo()
+        {
+            if (!string.IsNullOrWhiteSpace(NewTodo))
+            {
+                Todos.Add(new TodoItem
+                {
+                    Title = NewTodo
+                });
+            }
+        }
         
     }
 }
